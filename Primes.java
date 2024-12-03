d7d88faa728931e039aae7bda33f60769c8f847d
john-smith8888 <johnsmith8aug88@gmail.com>
2024-12-03 10:12:26 +0200

    primes 1st attempt

--- Primes.java
+++ Primes.java
@@ -1,5 +1,36 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        char v = args[1].charAt(0);
+        boolean verbose = v=='v';
+        int counter = 0;
+        System.out.println("Prime numbers up to " + n + ":");
+        // array initialization
+        boolean arr[] = new boolean[n+1];
+        for(int i=2;i<arr.length;i++){
+            arr[i] = true;
+        }
+
+        // corssing out primes by multilpiers
+        for (int i=2;i<arr.length;i++){
+            if (arr[i]) {
+                for (int j=2*i;j<=arr.length-1;j=j+i){
+                    arr[j] = false;
+                }
+                if (verbose) {
+                    System.out.println(i);
+                }
+            }
+        }
+        // counting primes
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]) {
+                counter++;
+            }
+        }
+
+        double ratio = 100*counter/n;
+        String pct = String.format("%,.0f", ratio);
+        System.out.println("There are " + counter + " primes between "+"2"+" and " + n + " ("+pct+"% are primes)");
     }
 }
